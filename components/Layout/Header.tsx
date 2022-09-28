b782bbae8b5573bfeacb81a7435bb8fc12578d5d
pfranczak <pfranczak@qualtrics.com>
2022-09-28 20:28:10 +0200

    FIx author's image

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="container mx-auto px-10 my-8 flex justify-between">
             <Link href="/">
                 <div className="flex cursor-pointer text-white">
-                        <Image className="rounded-full" src="/header-image.jpeg" alt="Author's portrait" width="50" height="50"/>
+                        <Image className="rounded-full" src="https://media.graphassets.com/8xnH2wp3TPWBBO04thTH" alt="Author's portrait" width="50" height="50"/>
                         <div className="ml-1.5">
                             <h1 className="font-bold text-lg title">Przemek Franczak</h1>
                             <h2 className="text-sm">Software Engineer</h2>
